e596a924c1baae53221b191271fcfd7a156a4103
Andrew <graduadu@gmail.com>
2021-02-10 15:19:13 +0200

    Added functionality adding to the shopping cart and displaying information and quantity of products in the cart

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/productModel');
+const Cart    = require('../models/cartModel');
 
 exports.getProducts = (req, res) => {
 	Product.fetchAll(products => {
@@ -20,4 +21,47 @@ exports.getProduct = (req, res) => {
 			path: '/products'
 		});
 	});
+}
+
+exports.getCart = (req, res) => {
+	Cart.getCart(cart => {
+		Product.fetchAll(products => {
+			const cartProducts = [];
+			for (product of products) {
+				const cartProductData = cart.products.find(cartProduct => cartProduct.id === product.id);
+				if (cartProductData) {
+					cartProducts.push({productData: product, qty: cartProductData.qty});
+				}
+			}
+			res.render('shop/cart.ejs', {
+				products: cartProducts,
+				pageTitle: "Your cart",
+				path: '/cart'
+			})
+		});
+	});
+
+
+}
+
+exports.postCart = (req, res) => {
+	const productId = req.body.productId;
+	Product.findById(productId, (product) => {
+		Cart.addProducts(productId, product.price);
+		res.redirect('/cart');
+	})
+}
+
+exports.getOrders = (req, res) => {
+	res.render('shop/orders.ejs', {
+		pageTitle: "Orders",
+		path: '/orders'
+	})
+}
+
+exports.getCheckout = (req, res) => {
+	res.render('shop/checkout.ejs', {
+		pageTitle: "Checkout",
+		path: '/checkout'
+	})
 }
--- routes/shopRouter.js
+++ routes/shopRouter.js
@@ -5,7 +5,9 @@ const shopController = require('../controllers/shopController');
 router.get('/', shopController.getProducts);
 router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
-router.get('/cart');
-router.get('/checkout');
+router.get('/cart', shopController.getCart);
+router.post('/cart', shopController.postCart);
+router.get('/checkout', shopController.getCheckout);
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
